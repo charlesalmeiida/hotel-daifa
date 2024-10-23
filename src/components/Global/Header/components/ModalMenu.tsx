7e130dfc048bbe119f6e23afb6941c6df026bfe9
charlesalmeiida <charlesalmeida844@gmail.com>
2024-10-23 18:21:22 -0300

    fix: fixed color on modal menu

--- src/components/Global/Header/components/ModalMenu.tsx
+++ src/components/Global/Header/components/ModalMenu.tsx
@@ -11,16 +11,16 @@ export function ModalMenu({ handleModal }: ModalMenuProps) {
       onMouseLeave={handleModal}
       className="bg-blue absolute -right-9 space-y-4 flex flex-col mt-px items-center rounded-[4px] shadow-shaddowShape py-4 px-11 transition-all"
     >
-      <ItemNav color="text-gray-50" link="/">
+      <ItemNav color="secondary" link="/">
         Galeria
       </ItemNav>
-      <ItemNav color="text-gray-50" link="/">
+      <ItemNav color="secondary" link="/">
         Localização
       </ItemNav>
-      <ItemNav color="text-gray-50" link="/">
+      <ItemNav color="secondary" link="/">
         Tour 360º
       </ItemNav>
-      <ItemNav color="text-gray-50" link="/">
+      <ItemNav color="secondary" link="/">
         FAQ
       </ItemNav>
     </div>
