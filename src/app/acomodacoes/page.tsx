59e36886e793f71695b5caee6e4ebab147cc1312
charlesalmeiida <charlesalmeida844@gmail.com>
2024-11-06 12:42:48 -0300

    feat: iniciate the gallerys swiper

--- src/app/acomodacoes/page.tsx
+++ src/app/acomodacoes/page.tsx
@@ -6,13 +6,13 @@ import { BtnRoom } from "@/components/Rooms/BtnRoom"
 import { useState } from "react"
 import roomsData from "@/app/data/roomsData.json"
 import { Footer } from "@/components/Global/Footer/Footer"
+import Image from "next/image"
 
 export default function Rooms() {
   const [isActive, setIsActive] = useState(false)
 
   const handleClick = () => {
     setIsActive(!isActive)
-    console.log()
   }
 
   return (
@@ -31,19 +31,30 @@ export default function Rooms() {
           </section>
 
           <section className="pt-20 pb-28">
-            <div className="flex gap-[102px] items-start">
-              <div className="flex flex-col items-start gap-10">
-                {roomsData.map((room) => (
-                  <BtnRoom
-                    key={room.title}
-                    handleClick={handleClick}
-                    isActive={isActive ? true : false}
-                  >
-                    {room.title}
-                  </BtnRoom>
-                ))}
+            <div className="flex justify-between">
+              <div className="flex gap-14 items-start">
+                <div className="flex flex-col items-start gap-10">
+                  {roomsData.map((room, index) => (
+                    <BtnRoom
+                      key={room.title}
+                      handleClick={handleClick}
+                      isActive={isActive && index === 0 ? true : false}
+                    >
+                      {room.title}
+                    </BtnRoom>
+                  ))}
+                </div>
+                <div className="w-px h-[456px] bg-gray-200"></div>
               </div>
-              <div className="w-px h-[456px] bg-gray-200"></div>
+              <div>
+                <Image
+                  className="rounded-[4px]"
+                  src={"/img/rooms/room01.png"}
+                  width={695}
+                  height={344}
+                  alt="Fotos dos quartos do Hotel Daifa"
+                />
+              </div>
             </div>
           </section>
         </Container>
