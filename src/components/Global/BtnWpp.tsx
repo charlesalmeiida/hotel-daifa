818d7265cccfe84587598d3987d83c58a91bb047
charlesalmeiida <charlesalmeida844@gmail.com>
2024-11-26 17:04:00 -0300

    refactor

--- src/components/Global/BtnWpp.tsx
+++ src/components/Global/BtnWpp.tsx
@@ -4,6 +4,7 @@ export function BtnWpp() {
   return (
     <div className="fixed bottom-5 z-30 right-5 md:bottom-10 md:right-10">
       <Link
+        className=""
         href={
           "https://wa.me//5548988643952?text=Olá,%20vim%20pelo%20website%20e%20gostaria%20de%20mais%20informações%20sobre%20reservas!"
         }
