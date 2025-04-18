1bfd5508d9b01d3175d99faad7e3da2c1e214976
Charles Almeida <charles_ap15@hotmail.com>
2025-04-18 11:20:07 -0300

    remove made by in footer

--- src/components/Footer/Copyright.tsx
+++ src/components/Footer/Copyright.tsx
@@ -7,16 +7,6 @@ export function Copyright() {
   return (
     <div className="flex flex-col gap-8 lg:gap-0 md:flex-row text-center justify-between mt-14 lg:mt-32 text-gray-300 font-mono text-base">
       <span className="block">{t("text")}</span>
-      <span className="block">
-        {t("developed")}{" "}
-        <Link
-          className="underline"
-          target="_blank"
-          href={"https://portfolio-charlesalmeiida.vercel.app/pt"}
-        >
-          Charles 
-        </Link>
-      </span>
     </div>
   )
 }
