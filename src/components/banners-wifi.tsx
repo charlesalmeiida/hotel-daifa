7fbd4fbf7e599c1ae8e2d54a89cfbe17d33ee3dd
Charles Almeida <charles_ap15@hotmail.com>
2025-04-17 23:39:33 -0300

    fix banner

--- src/components/banners-wifi.tsx
+++ src/components/banners-wifi.tsx
@@ -17,7 +17,7 @@ export function BannersWifi() {
       <Image
         src={isImageChange ? "/svg/banner-02.svg" : "/svg/banner-01.svg"}
         width={300}
-        className="absolute bottom-8 right-0 md:right-96"
+        className="absolute max-w-[280px] md:max-w-full bottom-8 left-3 md:left-auto md:right-96"
         height={50}
         alt="Wifi"
         onLoad={changeImageAfterDelay}
