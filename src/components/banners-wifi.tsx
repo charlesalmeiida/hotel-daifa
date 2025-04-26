fcb3c1549faac0d34877a2eca4bbdc0d29e5de00
Charles Almeida <charles_ap15@hotmail.com>
2025-04-26 10:38:19 -0300

    fix banner wifi position

--- src/components/banners-wifi.tsx
+++ src/components/banners-wifi.tsx
@@ -17,7 +17,7 @@ export function BannersWifi() {
       <Image
         src={isImageChange ? "/svg/banner-02.svg" : "/svg/banner-01.svg"}
         width={300}
-        className="max-w-[240px] md:max-w-full fixed bottom-5 z-30 left-5 md:bottom-10 md:left-10"
+        className="max-w-[220px] md:max-w-full fixed bottom-6 z-30 left-5 md:bottom-10 md:left-10"
         height={50}
         alt="Wifi"
         onLoad={changeImageAfterDelay}
