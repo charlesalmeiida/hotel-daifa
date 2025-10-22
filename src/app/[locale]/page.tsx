6e03fadf79d35e57c047bc9d7b38b8ba19ec009a
Criativo Lab <contato@criativolab.com.br>
2025-10-22 09:51:49 -0300

    Corrigindo erro de build

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import dynamic from "next/dynamic";
 
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Homepage";
-import { ModalPromotion, SimpleImageModal } from "@/components/ModalPromotion";
+import { SimpleImageModal } from "@/components/ModalPromotion";
 
 const TheHotel = dynamic(() => import("@/components/Homepage/the-hotel"));
 const PromotionsSection = dynamic(
